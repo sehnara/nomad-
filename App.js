e471c698cf4141c0e3ecb67c505213a037483732
sehoon <gsh723@douzone.com>
2021-11-29 23:11:32 +0900

    [2.8] navigationContainer 'theme(DefaultTheme, DarkTheme)'

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
+import useColorScheme from "react-native/Libraries/Utilities/useColorScheme";
+
 // 1. FONT
 import * as Font from "expo-font";
 import { FontAwesome5 } from "@expo/vector-icons";
@@ -9,18 +11,23 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 
 // 3. navigationContainer
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 import Tab from "./src/navigation/Tab";
 
 export default function Appr() {
   const [fonts] = Font.useFonts(FontAwesome5.font);
   const [assets] = useAssets([require("./asset/test.png")]);
+  const isDark = useColorScheme() == "dark";
 
   if (!fonts || !assets) {
     return <AppLoading />;
   } else {
     return (
-      <NavigationContainer>
+      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
         <Tab />
       </NavigationContainer>
     );
